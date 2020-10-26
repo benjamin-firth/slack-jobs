cd401bf9d4fe8851800757656100a37cff84fbe7
Ben Firth <bfirth93@gmail.com>
2020-10-26 11:29:37 -0600

    Add env variables

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,19 @@
+import React, { useSelect } from 'react';
 import './App.css';
 
 const App = () => {
+  const fetchConvo = async (url) => {
+    const response = await fetch(url);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  };
+
+  useSelect(() => {
+    fetchConvo('https://slack.com/api/conversations.history')
+  }, []);
+
   return (
     <main>
       <h1>Job Posts from Developer Slack Workspaces</h1>
